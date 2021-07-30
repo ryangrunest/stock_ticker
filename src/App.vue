ef2e30a3bfe12247e71df7099bb807b9f01483f9
Ryan Grunest <ryangrunest@geeko>
2021-07-30 15:25:09 -0700

    pushing starter code to github

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="
+      p-6
+      max-w-sm
+      mx-auto
+      bg-white
+      rounded-xl
+      shadow-md
+      flex
+      items-center
+      space-x-4
+    "
+  >
+    <div class="flex-shrink-0">
+      <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+    </div>
+    <div>
+      <div class="text-xl font-medium text-black">ChitChat</div>
+      <p class="text-gray-500">You have a new message!</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
